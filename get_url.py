834da5f6460f5d1f72706b6d5516af6cb8de2caf
Zdenek Hynek <zdenek.hynek@gmail.com>
2017-04-16 16:36:57 +0200

    Data exploration

--- get_url.py
+++ get_url.py
@@ -1,10 +1,11 @@
 import requests
+import simplejson as json
 
 
 def get_url(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        return response.content
+        return json.loads(response.content)
 
     return False
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import os
 
 from get_guardian_search import get_guardian_search
 from pymongo import MongoClient
+import time
 
 api_key = os.environ['GUARDIAN_API_KEY']
 query = 'terrorist'
@@ -12,5 +13,46 @@ db = client['capstone']
 articles_collection = db.articles
 
 
+def insert_article(article):
+    # using update to avoid duplication
+    query = {'id': article['id']}
 
-print get_guardian_search(query, api_key)
+    # add for which query article was returned
+    article['query'] = query
+
+    print 'Inserting id: %s' % (article['id'])
+    articles_collection.update(
+        query,
+        article,
+        True
+    )
+
+
+def insert_articles(articles):
+    map(insert_article, articles)
+
+
+def get_articles(page):
+    request_response = get_guardian_search(query, api_key, page)
+
+    if (request_response and request_response['response']['results']):
+        articles = request_response['response']['results']
+        pages = int(request_response['response']['pages'])
+
+        print 'Receive %s page from %s pages' % (page, pages)
+
+        #   store results
+        insert_articles(articles)
+
+        #   should we load additional articles?
+        if (pages and page < pages):
+            #   load next article
+            time.sleep(1)
+            get_articles(page + 1)
+
+    else:
+        print 'Nothing found'
+
+
+if __name__ == '__main__':
+    get_articles(210)
